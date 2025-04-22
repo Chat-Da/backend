8e1984df4f07d8d142f4ac89cb652d8fe024b801
Cutaku <mmzz0219@gmail.com>
2025-04-22 14:40:43 +0900

    feat : project upload

---
+++ chat-da/src/main/java/site/chatda/ChatDaApplication.java
@@ -0,0 +1,13 @@
+package site.chatda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatDaApplication.class, args);
+	}
+
+}
---
+++ chat-da/src/test/java/site/chatda/ChatDaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.chatda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
