bf58e4a546b30a62201449290d012650fb6d6155
Cutaku <mmzz0219@gmail.com>
2025-05-06 01:27:29 +0900

    feat : 엔티티 추가

---
+++ chat-da/src/main/java/site/chatda/domain/counsel/entity/TeacherComment.java
@@ -0,0 +1,29 @@
+package site.chatda.domain.counsel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+ class TeacherComment {
+
+    @Id
+    @Column(name = "counsel_id", columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "counsel_id", columnDefinition = "INT UNSIGNED")
+    private Report report;
+
+    @NotBlank
+    @Column(length = 500)
+    private String content;
+}
